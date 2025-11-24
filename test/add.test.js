8eb2d9270a9a86b404786f335afa115ef6e9d9b2
Olli Laaksonen <e12235144@student.tuwien.ac.at>
2025-11-24 21:36:29 +0200

    Local environment setup.

---
+++ test/add.test.js
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import add from "../util_lib/COMP.SE.200-2024-2025-1/src/add.js";
+
+describe("add()", () => {
+  it("adds two positive numbers", () => {
+    expect(add(2, 3)).to.equal(5);
+  });
+
+  it("adds negative numbers", () => {
+    expect(add(-5, -2)).to.equal(-7);
+  });
+
+  it("returns 0 if inputs are undefined", () => {
+    expect(add(undefined, undefined)).to.equal(0);
+  });
+
+  it("coerces strings to numbers", () => {
+    expect(add("4", "6")).to.equal(10);
+  });
+});
